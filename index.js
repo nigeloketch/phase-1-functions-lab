d75d1dcc600f4ec3103547eeba9f2504453cfaf1
Nigel <nigel.oketch@student.moringaschool.com>
2023-06-13 12:40:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(block) {
+	return block>42?(block-42):(42-block)
+}
+
+
+function distanceFromHqInFeet(block) {
+	return (distanceFromHqInBlocks(block))*264;
+}
+
+function distanceTravelledInFeet(start,arrive) {
+return (start<arrive)?(arrive-start)*264:(start-arrive)*264
+}
+
+function calculatesFarePrice(start,arrive) {
+	const distance = distanceTravelledInFeet(start,arrive)
+	if (distance < 400) {
+		return 0;
+	}
+	else if (distance >= 400 && distance <= 2000) {
+		return (distance - 400) * .02;
+	}
+	else if (distance > 2000 && distance < 2500) {
+		return 25;
+	}
+	else if (distance>2500){ 
+		return 'cannot travel that far';
+	}
+}
+
